ce62b0d60da405472b4d11c4c5aea19eebc5fc94
ltom <tom.liere@ynov.com>
2023-09-12 13:45:53 +0200

    création route

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const routes = require('./routes/routes.js');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/', routes);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
