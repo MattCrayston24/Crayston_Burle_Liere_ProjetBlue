e581d518bb82a27d1869da04e1323036d3ab7c90
ltom <tom.liere@ynov.com>
2023-09-14 09:47:47 +0200

    evenement 2.0

--- src/backend/javascript/jeux.js
+++ src/backend/javascript/jeux.js
@@ -1,42 +1,48 @@
-const questionsDiv = document.querySelector('.questions');
-let questionIndex = 0;
+fetch('http://localhost:3000/static/evenement.json')
+  .then(response => response.json())
+  .then(data => {
+    const questions = data.questions;
+    const questionsDiv = document.querySelector('.questions');
+    let questionIndex = 0;
 
-function afficherQuestion() {
-  fetch("../evenement.json")
-    .then(response => response.json())
-    .then(data => {
-      const questions = data.questions;
-      const questionCourante = questions[questionIndex];
-      if (questionCourante) {
-        const questionElement = document.createElement('div');
-        questionElement.className = 'question';
-        questionElement.innerHTML = `
-          <p>${questionCourante.text}</p>
-          <div class="choices">
-            ${questionCourante.choices.map(choice => `
-              <button onclick="repondre(${choice.id})">${choice.text}</button>
-            `).join('')}
-          </div>
-        `;
-        questionsDiv.innerHTML = ''; // Effacer le contenu précédent
-        questionsDiv.appendChild(questionElement);
-      } else {
-        // Toutes les questions ont été répondues
-        const finMessage = document.createElement('p');
-        finMessage.textContent = 'Toutes les questions ont été répondues. Le jeu est terminé.';
-        questionsDiv.appendChild(finMessage);
-      }
-    })
-    .catch(error => console.error(error));
-}
+    function afficherQuestion() {
+        const questionCourante = questions[questionIndex];
+        if (questionCourante) {
+            const questionElement = document.createElement('div');
+            questionElement.className = 'question';
+            questionElement.innerHTML = `
+                <p>${questionCourante.text}</p>
+                <div class="choices">
+                ${questionCourante.choices.map(choice => `
+                    <button onclick="repondre(${choice.id})">${choice.text}</button>
+                `).join('')}
+                </div>
+            `;
+            questionsDiv.innerHTML = '';
+            questionsDiv.appendChild(questionElement);
+        } else {
+            const finMessage = document.createElement('p');
+            finMessage.textContent = 'Toutes les questions ont été répondues. Le jeu est terminé.';
+            questionsDiv.appendChild(finMessage);
+        }
+    }
 
-function repondre(choiceId) {
-  // Le traitement de la réponse de l'utilisateur peut être ajouté ici, si nécessaire.
-  // Pour l'instant, nous n'avons pas de logique de traitement des réponses dans cette version simplifiée.
+    function repondre(choiceId) {
+        const questionCourante = questions[questionIndex];
+        if (questionCourante) {
+            const choix = questionCourante.choices.find(choice => choice.id === choiceId);
+            if (choix) {
+                const reponseElement = document.createElement('p');
+                reponseElement.textContent = choix.response;
+                questionsDiv.appendChild(reponseElement);
+                questionIndex++;
+                setTimeout(afficherQuestion, 1000); // Afficher la prochaine question après un délai d'1 seconde (vous pouvez ajuster le délai selon vos besoins)
+            }
+        }
+    }
 
-  questionIndex++;
-  afficherQuestion();
-}
-
-// Démarrer le jeu en affichant la première question
-afficherQuestion();
+    afficherQuestion();
+  })
+  .catch(error => {
+    console.error('Erreur lors du chargement du fichier JSON :', error);
+  });
